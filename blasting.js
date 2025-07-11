4a9b8db5fd037da298d005e682216fe0f80f77f3
Blockworldhd <107620354+BlockworldHD@users.noreply.github.com>
2025-07-11 22:43:59 +0200

    Deleted Files

--- blasting.js
+++
@@ -1,7 +0,0 @@
-//priority: 1
-//Create Live 5 Custom Recipes
-//Author: Blizzor
-
-ServerEvents.recipes((event) => {
-  event.recipes.blasting("create:powdered_obsidian", "minecraft:redstone");
-});
--- smelting.js
+++
@@ -1,9 +0,0 @@
-//priority: 1
-//Create Live 5 Custom Recipes
-//Author: Blizzor
-
-ServerEvents.recipes((event) => {
-  event.smelting("1x minecraft:quartz", "minecraft:sand");
-  event.smelting("1x minecraft:glass", "kubejs:glass_powder");
-  event.smelting("1x minecraft:quartz", "kubejs:quartz_dust");
-});
