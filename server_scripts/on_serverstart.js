a858f0797cf9beb600d29996ee5d8a02e0f4b7e8
Blockworldhd <107620354+BlockworldHD@users.noreply.github.com>
2025-07-12 10:55:40 +0200

    Delete server_scripts directory

--- server_scripts/on_serverstart.js
+++
@@ -1,4 +0,0 @@
-LevelEvents.loaded((event) => {
-  event.server.runCommandSilent(`skyblock manage teams clear`);
-  event.server.runCommandSilent(`gamerule doFireTick false`);
-});
--- server_scripts/sandpaper_polishing.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes((event) => {
-  event.recipes.create.sandpaper_polishing(
-    Item.of(
-      "minecraft:suspicious_gravel",
-      '{BlockEntityTag:{item:{Count:1b,id:"kubejs:lost_template"}}}'
-    ).withChance(0.025),
-    "minecraft:gravel"
-  );
-});
